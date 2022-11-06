c90121def145fef23d24210c03b7f138800fdc58
WalkerSumida <walkersumida@gmail.com>
2022-11-06 15:42:26 +0900

    refactor: remove unnecessary codes

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,15 @@ tests=\`go tool cover -func=cover.out\`
 failed=false
 
 echo "Threshold: $argThreshold"
+echo $tests
 
 while IFS= read -r t
 do
   num=\`echo $t | grep -Eo '[0-9]+\.[0-9]+'\`
   if (( $(echo "$num $argThreshold" | awk '{print ($1 < $2)}') )) ; then
-    COLOR=red;
     failed=true
     echo $t;
   else
-    COLOR=green;
     echo $t;
   fi
 done <<< "$tests"
